cef73c2aa24bfcb77a08c4d0ab97e2479a7a497c
Amman Abua <ammanabua@gmail.com>
2024-01-17 21:02:11 -0600

    Add files via upload

---
+++ Assignment2/Prog6.java
@@ -0,0 +1,21 @@
+public class Prog6 {
+
+    static int min(int[] arrayofInts) {
+
+        int min = 0;
+        for(int i = 0; i < arrayofInts.length; ++i){
+            if (arrayofInts[i] < min){
+                min = arrayofInts[i];
+            }
+        }
+
+        System.out.print("Minimum in the array: " + min);
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {-10, 5, 8, 22, -30, 40, -50};
+
+        Prog6.min(a);
+    }
+}
