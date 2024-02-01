1336ae443e0fcbfccf2b86100597b850438ac139
Amman Abua <ammanabua@gmail.com>
2024-01-31 18:10:01 -0600

    Add files via upload

--- Assignment9/Problem2/SinglyLinkedStack.java
+++ Assignment9/Problem2/SinglyLinkedStack.java
@@ -1,69 +1,6 @@
 package Assignment.Assignment9.Problem2;
 
 public class SinglyLinkedStack {
-//    class ListNode{
-//        Object data;
-//        ListNode next;
-//
-//        ListNode(Object item){
-//            data = item;
-//        }
-//    }
-//
-//    int count;
-//    ListNode head, top;
-//
-//
-//    public SinglyLinkedStack(){
-//        head = new ListNode(0);
-//    }
-//
-//    public void push(Object obj){
-//        ListNode toAdd = new ListNode(obj);
-//
-//        if (isEmpty()){
-//            top = toAdd;
-//            head.next = top;
-//            top.next = null;
-//        } else {
-//            toAdd.next = top;
-//            top = toAdd;
-//            top.next = null;
-//        }
-//
-//        count++;
-//    }
-//
-//    public Object pop(){
-//        if (isEmpty()){
-//            System.out.println("Stack is Empty");
-//            return null;
-//        }
-//        Object obj = top.data;
-//        top = top.next;
-//        count--;
-//        return obj;
-//    }
-//
-//    public Object peek(){
-//
-//        if(isEmpty()){
-//            System.out.println("Stack is Empty");
-//            return null;
-//        }
-//        return top.data;
-//    }
-//
-//    public boolean isEmpty(){
-//        return top == null;
-//    }
-//
-//
-//
-//    public int size(){
-//        return count;
-//    }
-
 
     class Node {
         Object data;
